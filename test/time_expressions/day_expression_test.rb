d8dc2855ab7dce26395a08cdcb8e46c37a2cd7eb
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2022-08-15 19:26:14 +0100

    [TEST] Add tests for DayExpression class

---
+++ test/time_expressions/day_expression_test.rb
@@ -0,0 +1,55 @@
+require "minitest/autorun"
+
+require "lib/time_expressions/day_expression"
+
+class DayExpressionTest < Minitest::Test
+
+  def test_can_handle_wildcard
+    expression = DayExpression.new('*')
+    assert_equal(
+      (1..31).to_a,
+      expression.occurrences
+    )
+  end
+
+  def test_can_handle_single_day
+    expression = DayExpression.new('12')
+    assert_equal(
+      [12],
+      expression.occurrences
+    )
+  end
+
+  def test_can_handle_range_of_days
+    expression = DayExpression.new('12-15')
+    assert_equal(
+      [12, 13, 14, 15],
+      expression.occurrences
+    )
+  end
+
+  def test_can_handle_comma_separated_days
+    expression = DayExpression.new('12,15,30')
+    assert_equal(
+      [12, 15, 30],
+      expression.occurrences
+    )
+  end
+
+  def test_can_handle_interval_of_days
+    expression = DayExpression.new('*/2')
+    assert_equal(
+      [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31],
+      expression.occurrences
+    )
+  end
+
+  def test_can_handle_comma_and_range_of_days
+    expression = DayExpression.new('1,2,3,9-11')
+    assert_equal(
+      [1, 2, 3, 9, 10, 11],
+      expression.occurrences
+    )
+  end
+
+end
