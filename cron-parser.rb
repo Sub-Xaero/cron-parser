b7b01bb953bc37211d01d1ee7f7958c5d4a2cb66
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2022-08-19 11:53:01 +0100

    [FEAT] Show next 5 instances

--- cron-parser.rb
+++ cron-parser.rb
@@ -1,5 +1,6 @@
 #!ruby
 
+require 'date'
 require './lib/time_expressions/minute_expression'
 require './lib/time_expressions/hour_expression'
 require './lib/time_expressions/day_expression'
@@ -19,6 +20,36 @@ hours = HourExpression.new(hour_expression).occurrences
 days = DayExpression.new(day_expression).occurrences
 months = MonthExpression.new(month_expression).occurrences
 weekdays = WeekdayExpression.new(weekday_expression).occurrences
+
+def next_date_instances(counter, minutes:, hours:, days:, months:, weekdays:)
+  now = DateTime.now
+  year = now.year
+
+  next_instances = []
+  loop do
+    months.each do |month|
+      days.each do |day|
+        hours.each do |hour|
+          minutes.each do |minute|
+            return next_instances if counter == 0
+
+            date = DateTime.new(year, month, day, hour, minute)
+            puts date
+            if date > now && weekdays.include?(date.wday)
+              next_instances << date
+              counter -= 1
+            end
+          end
+        end
+      end
+    end
+    year += 1
+  end
+
+  next_instances
+end
+
+next_instances = next_date_instances(5, minutes: minutes, hours: hours, days: days, months: months, weekdays: weekdays)
 
 def formatted_message(message, limit: 14)
   # If the given string is less than `limit` chars long, pad it, otherwise truncate it
@@ -40,3 +71,4 @@ field_output('day of month', days)
 field_output('month', months)
 field_output('day of week', weekdays)
 field_output('command', executable)
+field_output('next instances', next_instances.map(&:iso8601))
